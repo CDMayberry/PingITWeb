026e0830a7aebdba90472039fb336d1f03c6a331
CDMayberry <gmodman12@gmail.com>
2016-01-28 12:12:49 -0500

    test 4

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ session_start();
              <div class="col-lg-4">
                 <form id="formNotify" action="notify.php" method="post" class="form-horizontal">
                     <fieldset>
-                        <legend>Tester 44</legend>
+                        <legend>Tester 4123213</legend>
                         <div class="form-group">
                             <label for="inputEmail" class="col-lg-2 control-label">Email</label>
                             <div class="col-lg-10">
